c3b2708e16a0e5aff3aef7837540f6e5d4743e4e
Sammy Alvarado <isammyalvarado@gmail.com>
2021-07-11 20:22:13 -0400

    Added view Logic in VC

--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -10,13 +10,22 @@ import SpriteKit
 
 class ViewController: UIViewController {
     
+    // MARK: Property
+    var skScene: CustomScene? = nil
+    
     // MARK: Outlets
     @IBOutlet var skView: SKView!
     
     // MARK: View
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skScene = CustomScene(size: view.bounds.size)
+        skView.presentScene(skScene)
     }
     
     // MARK: Actions:
