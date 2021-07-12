a938da16d800b1488b2aac3e09c2d7a3556032dc
Sammy Alvarado <isammyalvarado@gmail.com>
2021-07-11 23:00:47 -0400

    Added logic to allow circle shape node to appear when screen is touched.

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -9,14 +9,43 @@ import UIKit
 import SpriteKit
 
 class CustomScene: SKScene {
+    
+    
+    
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
-            else { return }
+        else { return }
         
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        node.fillColor = randomUIColor()
         node.position = touch.location(in: self)
+    
+        
+        let zoomIn = SKAction.scale(by: 2.0, duration: 1)
+        let zoomOut = SKAction.scale(by: 0.8, duration: 0.5)
+//        let dismiss = SKAction.removeFromParent()
+        let zoomInZoomOUt = SKAction.sequence([zoomIn, zoomOut])
+        node.run(zoomInZoomOUt)
+        
+        
+        
         addChild(node)
+        
+    }
+    
+    private func randomColor() -> UIColor {
+        let red: CGFloat = CGFloat(drand48())
+        let green: CGFloat = CGFloat(drand48())
+        let blue: CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    private func randomUIColor() -> UIColor {
+        return .init(hue: .random(in: 0.5...1.0),
+                     saturation: 1,
+                     brightness: 1,
+                     alpha: 1)
     }
 }
 
